de05007604e7232d147b4a0e801852b58fc3de89
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-04 03:09:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ChakraProvider } from '@chakra-ui/react'; // Import ChakraProvider for Chakra UI
+import { ChakraProvider } from '@chakra-ui/react'; // For Chakra UI
+import { BrowserRouter } from 'react-router-dom';  // For routing
 
-// Initialize the React root element
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-// Render the app, wrapped with ChakraProvider for Chakra UI support
 root.render(
   <React.StrictMode>
-    <ChakraProvider> {/* Wrap the app with ChakraProvider to apply Chakra UI theming */}
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>  {/* Enables routing */}
+      <ChakraProvider>  {/* Wrap the app in ChakraProvider */}
+        <App />  {/* Main App Component */}
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
