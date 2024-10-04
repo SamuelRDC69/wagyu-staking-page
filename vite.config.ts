e98841eb10620bf308d6b000cbc1b4854a173a09
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-04 04:02:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    sourcemap: true, // Enable source maps for easier debugging
+    chunkSizeWarningLimit: 500, // Adjust chunk size limit if needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // This is optional and allows you to break large chunks into smaller ones
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Move vendor files to a separate chunk
+          }
+        },
+      },
+    },
+  },
 })
