ecf45e8e32f959eb525d1954d90b929cfd3ba5e9
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-28 22:01:51 +0000

    Update LeaderboardButton.tsx

--- src/components/LeaderboardButton.tsx
+++ src/components/LeaderboardButton.tsx
@@ -1,14 +1,16 @@
 // src/components/LeaderboardButton.tsx
-import { Button } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 const LeaderboardButton = () => {
   const navigate = useNavigate();
 
   return (
-    <Button colorScheme="blue" onClick={() => navigate('/leaderboard')}>
-      Go to Leaderboard
-    </Button>
+    <Flex justify="center" mt={8}>
+      <Button colorScheme="blue" size="lg" onClick={() => navigate('/leaderboard')} _hover={{ bg: 'blue.600' }}>
+        Go to Leaderboard
+      </Button>
+    </Flex>
   );
 };
 
