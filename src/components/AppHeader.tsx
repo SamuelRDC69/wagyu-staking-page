a49b2fa687e18f91046619605e2bc8e5c80b57b6
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-28 22:00:56 +0000

    Update AppHeader.tsx

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,13 @@
 // src/components/AppHeader.tsx
-import { Heading } from '@chakra-ui/react';
+import { Heading, Flex, Image } from '@chakra-ui/react';
 
 const AppHeader = () => (
-  <Heading as="h1" size="lg" textAlign="center" mt={8}>
-    Token Staking DApp
-  </Heading>
+  <Flex justify="center" align="center" p={4} bg="blue.900" color="white">
+    <Image src="/logo.svg" alt="App Logo" boxSize="40px" mr={3} />
+    <Heading as="h1" size="lg" textAlign="center">
+      Token Staking DApp
+    </Heading>
+  </Flex>
 );
 
 export default AppHeader;
