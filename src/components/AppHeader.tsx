89abd67e3e786c72cc6c95b0e8ce783a790722cd
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-28 00:20:44 +0000

    Update AppHeader.tsx

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,10 @@
+// src/components/AppHeader.tsx
+import { Heading } from '@chakra-ui/react';
+
+const AppHeader = () => (
+  <Heading as="h1" size="lg" textAlign="center" mt={8}>
+    Token Staking DApp
+  </Heading>
+);
+
+export default AppHeader;
