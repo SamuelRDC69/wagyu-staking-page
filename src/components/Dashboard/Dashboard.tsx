1c4d4eb9be00c4cc7fe77c1dec71e0fc2cca3389
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-29 23:27:33 +0000

    Update Dashboard.tsx

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,31 @@
+// src/components/Dashboard/Dashboard.tsx
+import React, { useContext } from 'react';
+import { VStack } from '@chakra-ui/react';
+
+import DashboardHeader from './DashboardHeader';
+import ClaimProjectionGauge from './ClaimProjectionGauge';
+import CooldownCountdown from './CooldownCountdown';
+import RiskMeter from './RiskMeter';
+import { UserContext } from '../../contexts/UserContext';
+import useBlockchainData from '../../hooks/useBlockchainData';
+
+const Dashboard: React.FC = () => {
+  const { session } = useContext(UserContext);
+
+  const { projectedRewards, cooldownEndTime, riskLevel } = useBlockchainData(session);
+
+  if (!session) {
+    return null;
+  }
+
+  return (
+    <VStack spacing={{ base: 4, md: 6 }} width="100%" px={{ base: 2, md: 4 }}>
+      <DashboardHeader />
+      <ClaimProjectionGauge projectedRewards={projectedRewards} />
+      <CooldownCountdown cooldownEndTime={cooldownEndTime} />
+      <RiskMeter riskLevel={riskLevel} />
+    </VStack>
+  );
+};
+
+export default Dashboard;
