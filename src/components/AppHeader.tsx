2ab052d4cd5f8d869d45469b15e81ca1f6d833cf
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-10-30 15:33:54 +0000

    Update AppHeader.tsx

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,6 +1,7 @@
 // src/components/AppHeader.tsx
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, HStack, Link as ChakraLink } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const AppHeader: React.FC = () => {
   return (
@@ -8,6 +9,14 @@ const AppHeader: React.FC = () => {
       <Heading as="h1" size="lg">
         Clash of Stakes
       </Heading>
+      <HStack spacing={8} justify="center" mt={4}>
+        <ChakraLink as={Link} to="/" color="white" fontWeight="bold">
+          Home
+        </ChakraLink>
+        <ChakraLink as={Link} to="/guilds" color="white" fontWeight="bold">
+          Guilds
+        </ChakraLink>
+      </HStack>
     </Box>
   );
 };
